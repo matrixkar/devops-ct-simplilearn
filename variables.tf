ca77c0ddfc4c7bbe47f3a6c06604b63469cc2564
Pablo Galeana Bailey <knoppixpgb@gmail.com>
2021-11-15 09:53:25 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {}
+
+variable "ami"  {}
+
+variable "vpc_id"  {}
+
+variable "public_subnet_id"  {}
